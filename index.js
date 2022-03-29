f6ff1afffc88c34e9a3fe88f7ad33818ca689cb4
Patty Chen <patty.chen1017@gmail.com>
2022-03-29 02:48:14 -0400

    commit tree c66a854b9530f7536d4e14d1be1be50969c78af5

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
+
+
+const random = Math.floor(Math.random() * 10);
+Number.parseInt(random,10);
+
+const num3 = 34;
+const num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(1,6,8,15,20)
